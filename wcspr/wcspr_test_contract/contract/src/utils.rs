33a56f3bd69dc0c7ac72ca518bdcc9dc4b181b82
Muhammad Talha Dar <muhammadtalhadar@scytalelabs.com>
2021-11-22 15:38:41 +0500

    implemented wcspr test proxy contract

---
+++ wcspr/wcspr_test_contract/contract/src/utils.rs
@@ -0,0 +1,39 @@
+use core::convert::TryInto;
+
+use alloc::{
+    format,
+    string::String,
+};
+
+use casper_contract::{
+    contract_api::{runtime, storage},
+    unwrap_or_revert::UnwrapOrRevert,
+};
+
+use casper_types::{
+    bytesrepr::{ToBytes, FromBytes},
+    CLTyped
+};
+
+pub fn get_key<T: FromBytes + CLTyped + Default>(name: &str) -> T {
+    match runtime::get_key(name) {
+        None => Default::default(),
+        Some(value) => {
+            let key = value.try_into().unwrap_or_revert();
+            storage::read(key).unwrap_or_revert().unwrap_or_revert()
+        }
+    }
+}
+
+pub fn set_key<T: ToBytes + CLTyped>(name: &str, value: T) {
+    match runtime::get_key(name) {
+        Some(key) => {
+            let key_ref = key.try_into().unwrap_or_revert();
+            storage::write(key_ref, value);
+        }
+        None => {
+            let key = storage::new_uref(value).into();
+            runtime::put_key(name, key);
+        }
+    }
+}
