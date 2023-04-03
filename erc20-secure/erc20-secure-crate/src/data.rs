b07ee63b0c559aa93404421862ff38a81de05954
BassamMunib <bassam.munib@scytalelabs.com>
2023-04-03 17:48:36 +0500

    redundant admin fix

--- erc20-secure/erc20-secure-crate/src/data.rs
+++ erc20-secure/erc20-secure-crate/src/data.rs
@@ -1,9 +1,7 @@
-use casper_types::{ContractHash, ContractPackageHash, Key};
+use casper_types::{ContractHash, ContractPackageHash};
 use casperlabs_contract_utils::{get_key, set_key};
-use common::functions::zero_address;
 pub const SELF_CONTRACT_HASH: &str = "self_contract_hash";
 pub const CONTRACT_PACKAGE_HASH: &str = "contract_package_hash";
-pub const ADMIN: &str = "admin";
 
 pub fn set_contract_hash(contract_hash: ContractHash) {
     set_key(SELF_CONTRACT_HASH, contract_hash);
@@ -19,12 +17,4 @@ pub fn set_package_hash(package_hash: ContractPackageHash) {
 
 pub fn get_package_hash() -> ContractPackageHash {
     get_key(CONTRACT_PACKAGE_HASH).unwrap_or_default()
-}
-
-pub fn set_admin(admin: Key) {
-    set_key(ADMIN, admin);
-}
-
-pub fn get_admin() -> Key {
-    get_key(ADMIN).unwrap_or_else(zero_address)
 }
